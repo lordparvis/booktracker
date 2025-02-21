e8f88b6e48dd445a7c69fdc73f7477b5e03b3412
Parvis H <phaydark@gmail.com>
2025-02-20 20:32:24 -0700

    Register Book model in admin and add __str__ for better display

--- books/models.py
+++ books/models.py
@@ -3,3 +3,7 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=120)
     status = models.CharField(max_length=20, choices=[("reading","Reading"),("read","Read"),("toread","Will Read")])
+
+    def __str__(self):
+        return self.title
+    

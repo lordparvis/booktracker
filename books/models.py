73f2f11e40c48aaf5e4076c4b49436128aa51e8a
Parvis H <phaydark@gmail.com>
2025-02-20 20:11:11 -0700

    Add Book model with title and status including toread

--- books/models.py
+++ books/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=120)
+    status = models.CharField(max_length=20, choices=[("reading","Reading"),("read","Read"),("toread","Will Read")])
